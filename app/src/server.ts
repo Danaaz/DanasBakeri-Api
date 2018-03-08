b3f638c244231d4cb40cebe0850964a83aefc579
Dana Zangana <danaaz@student.matnat.uio.no>
2018-03-08 13:47:19 +0100

    update gitignore

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,11 +1,10 @@
-import express from 'express';
-import {Request, Response} from 'express';
+import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) =>{
+app.get("/", (req: Request, res: Response) => {
     res.json({"Name": "Applitude"});
 });
 
 app.listen(3000, () => console.log("Running on port 3000"));
-
